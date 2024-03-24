a2fbabbc2cc97eaf98ae8ea76f72b41f412768ed
gabriel-augusto <89670793+GabrielAraujodev@users.noreply.github.com>
2024-03-23 21:27:27 -0300

    skills

--- src/app/page/sobre/sobre.component.ts
+++ src/app/page/sobre/sobre.component.ts
@@ -4,9 +4,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
-  styleUrls: ['./sobre.component.css']  // Corrigi o nome da propriedade para 'styleUrls'
+  styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent implements OnInit {
+Html5: any;
+
+
 
   constructor() { }
 
@@ -17,5 +20,16 @@ export class SobreComponent implements OnInit {
   image1 = './assets/gmail.jpg';
   image2 = './assets/linkedin.jpg';
   corriculo = './assets/Currículo.pdf';
-  code = './assets/code.jpg'
+  code = './assets/code.jpg';
+  java ='./assets/Java.jpg';
+  angular = './assets/AngularJS.jpg';
+  cloud = './assets/Cloud.jpg';
+  ionic ='./assets/Ionic.jpg';
+  javascript = './assets/JavaScript.jpg';
+  sql =   './assets/SQL.jpg';
+  saas = './assets/Sass.jpg';
+  html = './assets/Html5.jpg';
+  fire = './assets/Firebase.jpg';
+
+
 }
